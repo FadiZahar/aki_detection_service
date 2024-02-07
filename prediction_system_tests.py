bd7cbb620209e1b79344a08aa6bee95f7408988f
khigashinosg <108298890+khigashinosg@users.noreply.github.com>
2024-02-07 12:02:56 +0000

    created prediction_system_tests.py, adding just message examples

---
+++ prediction_system_tests.py
@@ -0,0 +1,10 @@
+admit_message_example = ["MSH|^~\&|SIMULATION|SOUTH RIVERSIDE|||20240328132400||ADT^A01|||2.5",
+                         "PID|1||444129||AYAT BURKE||19940216|F"]
+
+discharge_message_example = ["MSH|^~\&|SIMULATION|SOUTH RIVERSIDE|||20240331035800||ADT^A03|||2.5",
+                             "PID|1||829339"]
+
+creatinine_message_example = ["MSH|^~\&|SIMULATION|SOUTH RIVERSIDE|||20240331003200||ORU^R01|||2.5",
+                              "PID|1||125412",
+                              "OBR|1||||||20240331003200",
+                              "OBX|1|SN|CREATININE||127.5695463720204]"]
