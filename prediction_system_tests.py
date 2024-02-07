3b676dbde619ca972251d14423c55c3d5675a44c
khigashinosg <108298890+khigashinosg@users.noreply.github.com>
2024-02-07 17:08:34 +0000

    changed prediction_system_tests

--- prediction_system_tests.py
+++ prediction_system_tests.py
@@ -9,28 +9,22 @@ creatinine_message_example = ["MSH|^~\&|SIMULATION|SOUTH RIVERSIDE|||20240331003
                               "OBR|1||||||20240331003200",
                               "OBX|1|SN|CREATININE||127.5695463720204]"]
 
-from prediction_system import preload_history, extract_type_and_mrn, extract_features
+from prediction_system import preload_history
 
 def test_preload_history():
     dataframe = preload_history()
+    print(dataframe.head(10))
     correct_column_names = ["age", "sex", "test_1", "test_2", "test_3", "test_4", "test_5"]
     actual_columns = [col[0] for col in dataframe.columns.tolist()]
     assert correct_column_names == actual_columns # check column names
     assert (dataframe["age"].nunique() == 0).all() # check all age values are None
     assert (dataframe["sex"].nunique() == 0).all() # check all sex values are None
 
-def test_extract_type_and_mrn():
-    assert extract_type_and_mrn(admit_message_example) == ("ADT^A01", "444129")
-    assert extract_type_and_mrn(discharge_message_example) == ("ADT^A03", "829339")
-    assert extract_type_and_mrn(creatinine_message_example) == ("ORU^R01", "125412")
-
 def test_extract_features():
     pass
 
 def main():
     test_preload_history()
-    test_extract_type_and_mrn()
-    
     
 if __name__ == "__main__":
     main()
