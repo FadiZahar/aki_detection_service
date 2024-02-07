a30e1a6b5e076a33e26d3383707b613c0107e1fd
khigashinosg <108298890+khigashinosg@users.noreply.github.com>
2024-02-07 12:09:44 +0000

    created extract_type_and_mrn tests in prediction_system_tests.py, all working

--- prediction_system_tests.py
+++ prediction_system_tests.py
@@ -8,3 +8,19 @@ creatinine_message_example = ["MSH|^~\&|SIMULATION|SOUTH RIVERSIDE|||20240331003
                               "PID|1||125412",
                               "OBR|1||||||20240331003200",
                               "OBX|1|SN|CREATININE||127.5695463720204]"]
+
+from prediction_system import extract_type_and_mrn
+
+def test_extract_type_and_mrn():
+    assert extract_type_and_mrn(admit_message_example) == ("ADT^A01", "444129")
+    assert extract_type_and_mrn(discharge_message_example) == ("ADT^A03", "829339")
+    assert extract_type_and_mrn(creatinine_message_example) == ("ORU^R01", "125412")
+
+
+
+def main():
+    test_extract_type_and_mrn()
+    
+if __name__ == "__main__":
+    main()
+    print("All tests passed!")
